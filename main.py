2e41f73e67c7d3a05d3d11832d348214a325b4f8
Waterlaw <abkerimov.e.i20@gmail.com>
2022-12-09 01:26:18 +0300

    add search panel

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify, json
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify, json, g
 from flask_mysqldb import MySQL, MySQLdb
 import re
 import datetime
 from passlib.hash import sha256_crypt
+from functools import wraps
 
 from db_CRUD import execute_read_query, execute_query
 
 app = Flask(__name__)
 
-app.secret_key = 'super secret key'
+app.secret_key = 'KAMD!)IR#(J@*U*$(@)!JJDS'
 app.permanent_session_lifetime = datetime.timedelta(seconds=600)
 mysql = MySQL(app)
 
@@ -32,97 +33,42 @@ def create_connection(host, user, password, db):
 
 
 connect_db = create_connection('localhost', 'root', 'root', 'sofa_shop')
-# connect_db = create_connection('waterlaw1.mysql.pythonanywhere-services.com', 'waterlaw1', 'elmir2022', 'waterlaw1$sofa_shop')
-
-# Show lesson on page
-@app.route('/reg', methods=['GET', 'POST'])
-def reg():
-    msg = ''
-    if request.method == 'POST':
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        email = request.form['email']
-        password = sha256_crypt.encrypt(request.form['password'])
-
-        check_sql = f'''select * from user where email = "{email}"'''
-        account = execute_read_query(connect_db, check_sql)
-
-        if account:
-            msg = 'Этот никнейм уже занят.'
-        # elif len(request.form['nickname']) < 4:
-        #     msg = 'Никнейм должен содержать не менее 4 символов.'
-        # elif not re.match(r'[A-Za-z]', nickname):
-        #     msg = 'Никнейм может содержать только латинские буквы.'
-        elif not re.match(r'[А-Яа-я]', first_name or last_name):
-            msg = 'Имя и фамилия могут содержать только кириллицу.'
-        elif first_name[0].islower() or last_name[0].islower():
-            msg = 'Имя и фамилия должны начинаться с заглавных букв.'
-        elif len(request.form['password']) < 8:
-            msg = 'Пароль должен содержать не менее 8 символов.'
-        else:
-            write_sql = f'''INSERT INTO `user` (`email`, `password`, `first_name`, `last_name`) 
-            VALUES ('{email}', '{password}', '{first_name}', '{last_name}')'''
-            execute_query(connect_db, write_sql)
-            return redirect(url_for('login'))
-    return render_template('registration.html', msg=msg)
 
 
-# Registration
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    # print(request.url_root + 'login')
-    if (request.referrer != request.url_root + 'login') and (request.referrer != request.url_root + 'reg'):
-        session['request'] = request.referrer
+# connect_db = create_connection('waterlaw1.mysql.pythonanywhere-services.com', 'waterlaw1', 'elmir2022',
+#                                'waterlaw1$sofa_shop')
 
-    print(session.get('request'))
-    msg = ''
-    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
-        email = request.form['email']
-        password = request.form['password']
 
-        check_sql = f'''select * from user where email = "{email}"'''
-        login_user = execute_read_query(connect_db, check_sql)
-        if login_user == tuple():
-            msg = 'Неверный никнейм или пароль.'
-        else:
-            login_user = login_user[0]
-            if sha256_crypt.verify(password, login_user['password']):
-                # if login_user['teacher']:
-                #     session['logged_in'] = True
-                #     session['id'] = login_user['id']
-                #     session['first_name'] = login_user['first_name']
-                #     session['nickname'] = login_user['email']
-                #     return redirect(url_for('check_lessons'))
-                # else:
-                session['logged_in'] = True
-                session['first_name'] = login_user['first_name']
-                session['last_name'] = login_user['last_name']
-                session['nickname'] = login_user['email']
-                session['user_id'] = login_user['id']
-                if session.get('request') != None:
-                    return redirect(session.get('request'))
-                return redirect(url_for('home'))
-            else:
-                msg = 'Неверный никнейм или пароль.'
-    return render_template('login.html', msg=msg)
+# Search decorator
+def search(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if request.args.get('search_value'):
+            value = request.args.get('search_value')
+            check_sql = f'''SELECT id, color_id, picture, product.title, product.price, color_id 
+                        FROM product, product_has_color where count>0 
+                        and product_has_color.product_id = product.id and product.title LIKE "%{value}%" group by id'''
+            products = execute_read_query(connect_db, check_sql)
+            print(products)
+            return render_template('search.html', sess_login=session, search_value=value, products=products)
+        return f(*args, **kwargs)
+
+    return decorated_function
 
 
 # Login
 @app.route('/')
+@search
 def home():
     check_sql = f'''SELECT id, color_id, picture, product.title, product.price, color_id
                 FROM product, product_has_color where count>0 
                 and product_has_color.product_id = product.id group by id'''
     products = execute_read_query(connect_db, check_sql)
-    print(products)
-    print(session)
-    # if session.get('logged_in'):
-    #     session_login = session['logged_in']
-    #     return render_template('home.html', products=products, sess_login=session['logged_in'])
     return render_template('home.html', products=products, sess_login=session)
 
 
 @app.route("/product/<int:id>/<int:color_id>", methods=['GET', 'POST'])
+@search
 def product(id, color_id):
     product_in_order = None
     msg = ''
@@ -139,14 +85,12 @@ def product(id, color_id):
         return '<h1>Invalid link</h1>'
     else:
         product_data = product_data[0]
-
-    print(product_data)
     check_picture_product = f'''Select product.id, color.id as color_id, picture from type, product, color, product_has_color
                             Where product.id=product_has_color.product_id 
                             and color.id=product_has_color.color_id
                             and product.id={id}'''
     product_picture = execute_read_query(connect_db, check_picture_product)
-    print(product_picture)
+    # print(product_picture)
     if session.get('logged_in'):
         check_actual_order = f'''SELECT id, actual FROM orders
                                                     where idUser = {session.get('user_id')} and actual = 1'''
@@ -163,12 +107,9 @@ def product(id, color_id):
                 msg = 'Товар добавлен в корзину'
         if request.method == 'POST':
             if actual_order == tuple():  # ORDER НЕ СУЩЕВСТВУЕТ
-
-                print('Order неактуален')
                 create_order = f'''INSERT INTO `orders` (`idUser`, `datetime`, `actual`)
                                VALUES ('{session.get('user_id')}', curdate(), 1)'''
                 id_actual_order = execute_query(connect_db, create_order)
-                print(f"{id_actual_order} Order создан")
             check_product_in_order = f'''select * from order_product where idOrder = {id_actual_order}
                                      and idProduct = {id} and color_id = {color_id}'''
             product_add_order = execute_read_query(connect_db, check_product_in_order)
@@ -178,24 +119,13 @@ def product(id, color_id):
                 execute_query(connect_db, insert_product)
                 product_in_order = True
                 msg = 'Товар добавлен в корзину'
-
-        # if product_add_order != tuple():
-        #     msg = 'Товар в корзине'
-
-        # msg = 'Товар добавлен в корзину'
-        # print(msg)
-        # try:
-        #     pass
-        # except:
-        #     pass
-        #     msg = 'Возникли неполадки на сервере, повторите позже'
-        print(product_in_order)
     return render_template('product.html', pro_item=product_data, pro_pic=product_picture,
                            sess_login=session, msg=msg, product_in_order=product_in_order,
                            color_id=color_id)
 
 
 @app.route("/cart")
+@search
 def cart():
     msg = ''
     if session.get('logged_in'):
@@ -212,7 +142,6 @@ def cart():
             check_total_price = f'''SELECT sum(order_product.count*price) as total_price FROM order_product, product 
                             where idOrder = '{id_actual}' and idProduct=product.id'''
             total_price = str(execute_read_query(connect_db, check_total_price)[0]['total_price']) + ' ₽'
-            print(total_price)
             check_total_count = f'''SELECT sum(order_product.count) as total_count FROM order_product 
                                         where idOrder="{id_actual}"'''
             total_count = execute_read_query(connect_db, check_total_count)[0]['total_count']
@@ -281,13 +210,74 @@ def exit_account():
     return redirect(session.get('request'))
 
 
-@app.route("/test", methods=["GET", "POST"])
-def test():
-    if request.method == "POST":
-        msg = request.form.get("todo")
-        print(msg)
-        return jsonify({'msg': msg})
-    return render_template('test.html')
+@app.route('/reg', methods=['GET', 'POST'])
+def reg():
+    msg = ''
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        password = sha256_crypt.encrypt(request.form['password'])
+
+        check_sql = f'''select * from user where email = "{email}"'''
+        account = execute_read_query(connect_db, check_sql)
+
+        if account:
+            msg = 'Этот никнейм уже занят.'
+        # elif len(request.form['nickname']) < 4:
+        #     msg = 'Никнейм должен содержать не менее 4 символов.'
+        # elif not re.match(r'[A-Za-z]', nickname):
+        #     msg = 'Никнейм может содержать только латинские буквы.'
+        elif not re.match(r'[А-Яа-я]', first_name or last_name):
+            msg = 'Имя и фамилия могут содержать только кириллицу.'
+        elif first_name[0].islower() or last_name[0].islower():
+            msg = 'Имя и фамилия должны начинаться с заглавных букв.'
+        elif len(request.form['password']) < 8:
+            msg = 'Пароль должен содержать не менее 8 символов.'
+        else:
+            write_sql = f'''INSERT INTO `user` (`email`, `password`, `first_name`, `last_name`) 
+            VALUES ('{email}', '{password}', '{first_name}', '{last_name}')'''
+            execute_query(connect_db, write_sql)
+            return redirect(url_for('login'))
+    return render_template('registration.html', msg=msg)
+
+
+# Registration
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # print(request.url_root + 'login')
+    if (request.referrer != request.url_root + 'login') and (request.referrer != request.url_root + 'reg'):
+        session['request'] = request.referrer
+    msg = ''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+
+        check_sql = f'''select * from user where email = "{email}"'''
+        login_user = execute_read_query(connect_db, check_sql)
+        if login_user == tuple():
+            msg = 'Неверный никнейм или пароль.'
+        else:
+            login_user = login_user[0]
+            if sha256_crypt.verify(password, login_user['password']):
+                # if login_user['teacher']:
+                #     session['logged_in'] = True
+                #     session['id'] = login_user['id']
+                #     session['first_name'] = login_user['first_name']
+                #     session['nickname'] = login_user['email']
+                #     return redirect(url_for('check_lessons'))
+                # else:
+                session['logged_in'] = True
+                session['first_name'] = login_user['first_name']
+                session['last_name'] = login_user['last_name']
+                session['nickname'] = login_user['email']
+                session['user_id'] = login_user['id']
+                if session.get('request') != None:
+                    return redirect(session.get('request'))
+                return redirect(url_for('home'))
+            else:
+                msg = 'Неверный никнейм или пароль.'
+    return render_template('login.html', msg=msg)
 
 
 if __name__ == '__main__':
